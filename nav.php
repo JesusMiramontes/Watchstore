826015443d5ce759e450aa2ebf8f99623d1e367f
Jesus Miramontes <jesus_miramontes@me.com>
2018-04-08 18:37:27 -0500

    Orders

--- nav.php
+++ nav.php
@@ -12,10 +12,7 @@
                     <a class="nav-link js-scroll-trigger" href="./store.php">Our products</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#download">Download</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+                    <a class="nav-link js-scroll-trigger" href="./orders.php">My orders</a>
                 </li>
                 <li class="nav-item" style="padding-top: 8px; padding-left: 7px">
                     <a href="cart.php"><i class="fas fa-shopping-cart"></i></a>
---
+++ orders.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include("./header.php"); ?>
+</head>
+
+<body class="bg-light" id="page-top">
+<!-- Navigation -->
+<?php include("nav.php")?>
+
+<?php include("./banners/1.php")?>
+
+
+<!-- Intro Header -->
+<header class="masthead" style="margin-top: 0%; ">
+    <div class="intro-body">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 mx-auto">
+                    <h1 class="brand-heading">Before time</h1>
+                    <div style="font-size: 6em"><i class="fas fa-shopping-cart"></i></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<!-- inicio orden uno-->
+<!-- Intro Header -->
+<div class="intro-body">
+    <div class="container" style="margin-top: 200px">
+        <h2>Order #71273567</h2>
+        <div class="row">
+
+
+            <div class="col-lg-12 col-md-6 mb-4" >
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <a href="my_default_address.php">Address</a>
+                        </h4>
+                        <h5 style="color: black !important;"><b>Col:</b> Canal del Oeste. <br><b>Número: </b> 56172-A <br> <b>Código postal: </b>27000</h5>
+                        <p class="card-text" style="color: black !important;"><b>Recibe:</b> Jesús Miramontes</p>
+                    </div>
+                    <div class="card-footer white-over-black" style="">
+                        <h6 style=""><b>Status:</b> En camino</h6>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-lg-4 col-md-6 mb-4" >
+                <div class="card h-100">
+                    <a href="#"><img class="card-img-top" src="./media/images/ressence/1.png" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <a href="item_1.php">Item One</a>
+                        </h4>
+                        <h5 style="color: black !important;">$24.99</h5>
+                        <p class="card-text" style="color: black !important;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card h-100">
+                    <a href="#"><img class="card-img-top" src="./media/images/ressence/2.png" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <a href="#">Item Two</a>
+                        </h4>
+                        <h5 style="color: black !important;">$34.99</h5>
+                        <p class="card-text" style="color: black !important;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card h-100">
+                    <a href="#"><img class="card-img-top" src="./media/images/ressence/3.png" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <a href="#">Item Two</a>
+                        </h4>
+                        <h5 style="color: black !important;">$44.99</h5>
+                        <p class="card-text" style="color: black !important;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-12 col-md-6 mb-4" >
+                <div class="card">
+                    <h1 class="total">Total: $5500 USD</h1>
+                    <form class=""><button class="btn proceed-to-checkout white-over-black" type="submit" formaction="/action_page2.php">Solicitar ayuda</button></form>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+<!-- fin de orden uno-->
+<div class="intro-body">
+    <div class="container" style="margin-top: 200px">
+        <h2>Order #91273567</h2>
+        <div class="row">
+
+
+            <div class="col-lg-12 col-md-6 mb-4" >
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <a href="my_default_address.php">Address</a>
+                        </h4>
+                        <h5 style="color: black !important;"><b>Col:</b> Canal del Oeste. <br><b>Número: </b> 56172-A <br> <b>Código postal: </b>27000</h5>
+                        <p class="card-text" style="color: black !important;"><b>Recibe:</b> Jesús Miramontes</p>
+                    </div>
+                    <div class="card-footer white-over-black" style="">
+                        <h6 style=""><b>Status:</b> Envío pendiente</h6>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-lg-4 col-md-6 mb-4" >
+                <div class="card h-100">
+                    <a href="#"><img class="card-img-top" src="./media/images/ressence/1.png" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <a href="item_1.php">Item One</a>
+                        </h4>
+                        <h5 style="color: black !important;">$24.99</h5>
+                        <p class="card-text" style="color: black !important;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 mb-4" >
+                <div class="card h-100">
+                    <a href="#"><img class="card-img-top" src="./media/images/ressence/1.png" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <a href="item_1.php">Item One</a>
+                        </h4>
+                        <h5 style="color: black !important;">$24.99</h5>
+                        <p class="card-text" style="color: black !important;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card h-100">
+                    <a href="#"><img class="card-img-top" src="./media/images/ressence/2.png" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <a href="#">Item Two</a>
+                        </h4>
+                        <h5 style="color: black !important;">$34.99</h5>
+                        <p class="card-text" style="color: black !important;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card h-100">
+                    <a href="#"><img class="card-img-top" src="./media/images/ressence/3.png" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <a href="#">Item Two</a>
+                        </h4>
+                        <h5 style="color: black !important;">$44.99</h5>
+                        <p class="card-text" style="color: black !important;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-12 col-md-6 mb-4" >
+                <div class="card">
+                    <h1 class="total">Total: $7500 USD</h1>
+                    <form class=""><button class="btn proceed-to-checkout white-over-black" type="submit" formaction="/action_page2.php">Solicitar ayuda</button></form>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+
+<br>
+<br>
+
+<?include("footer.php")?>
+</body>
+
+</html>
