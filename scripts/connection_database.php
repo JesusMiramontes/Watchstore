854e24a0731ad5cb995569617395f44c1512df6e
Jesus Miramontes <jesus_miramontes@me.com>
2018-04-26 15:41:27 -0500

    Conexion a la base de datos

---
+++ scripts/connection_database.php
@@ -0,0 +1,19 @@
+<?php
+
+# Si no existe, crea una sesión.
+if(!isset($_SESSION)){
+    session_start();
+}
+
+# Almacena la conexión abierta a la base de datos.
+global $connection;
+
+
+# Configuración para la conexion.
+$host = "127.0.0.1";
+$username = "root";
+$password = "";
+$database_name = "watchstore";
+
+# Crea la conexcion y se almacena en la variable global previamente definidgit 
+$connection = mysqli_connect($host, $username, $password, $database_name);
