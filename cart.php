07ebf26eb70cdeb39cbe2dff5c311e847ce0b9ad
Iván <j.ivan.sifuentes.v@gmail.com>
2018-05-29 19:43:49 -0500

    Modifique el archivo cart en la raiz para mostrar el carrito de forma dinamica

--- cart.php
+++ cart.php
@@ -27,66 +27,48 @@
 </header>
 
 <!-- Intro Header -->
-    <div class="intro-body">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4 col-md-6 mb-4" style="margin-top: 200px">
-                    <div class="card h-100">
-                        <a href="#"><img class="card-img-top" src="./media/images/ressence/1.png" alt=""></a>
-                        <div class="card-body">
-                            <h4 class="card-title">
-                                <a href="item_1.php">Item One</a>
-                            </h4>
-                            <h5 style="color: black !important;">$24.99</h5>
-                            <p class="card-text" style="color: black !important;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                        </div>
-                        <div class="card-footer">
-                            <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 mb-4" style="margin-top: 200px">
-                    <div class="card h-100">
-                        <a href="#"><img class="card-img-top" src="./media/images/ressence/2.png" alt=""></a>
-                        <div class="card-body">
-                            <h4 class="card-title">
-                                <a href="#">Item Two</a>
-                            </h4>
-                            <h5 style="color: black !important;">$34.99</h5>
-                            <p class="card-text" style="color: black !important;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                        </div>
-                        <div class="card-footer">
-                            <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 mb-4" style="margin-top: 200px">
-                    <div class="card h-100">
-                        <a href="#"><img class="card-img-top" src="./media/images/ressence/3.png" alt=""></a>
-                        <div class="card-body">
-                            <h4 class="card-title">
-                                <a href="#">Item Two</a>
-                            </h4>
-                            <h5 style="color: black !important;">$44.99</h5>
-                            <p class="card-text" style="color: black !important;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                        </div>
-                        <div class="card-footer">
-                            <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                        </div>
-                    </div>
-                </div>
+<header class="masthead" style="margin-top: 45%; background: white">
+        <div class="intro-body">
+            <div class="container">
+                <div class="row">
 
-                <div class="col-lg-4 col-md-6 mb-4" >
-                    <div class="card">
-                        <h1 class="total">Total: $5500 USD</h1>
-                        <form class="proceed-to-checkout"><button class="btn proceed-to-checkout" type="submit" formaction="/action_page2.php">Proceder al pago</button></form>
-                    </div>
-                </div>
 
+
+                    <?php
+                    include("scripts/connection_database.php");
+                    SESSION_START();
+                    $iduser = $_SESSION['id'];
+
+                    $sql = "SELECT idproducts, nombre, descripcion, img, stock, precio FROM cart WHERE iduser = '$iduser';";
+                    $result = $connection->query($sql);
+
+                    if($result->num_rows > 0)
+                    {
+                        while($row = $result->fetch_assoc())
+                        {
+                            echo "<div class=\"col-lg-4 col-md-6 mb-4\" style=\"margin-top: 200px\">";
+                            echo "<div class=\"card h-100\">";
+                            echo "<a href=\"#\"><img class=\"card-img-top\" src=\"{$row['img']}\" alt=\"\"></a>";
+                            echo "<div class=\"card-body\">";
+                            echo "<h4 class=\"card-title\">";
+                            echo "<a href=\"\">{$row['nombre']}</a>";
+                            echo "</h4>";
+                            echo "<h5 style=\"color: black !important;\">$ {$row['precio']}</h5>";
+                            echo "<p class=\"card-text\" style=\"color: black !important;\">{$row['descripcion']}</p>";
+                            echo "</div>";
+                            echo "<div class=\"card-footer\">";
+                            echo "<small class=\"text-muted\">&#9733; &#9733; &#9733; &#9733; &#9734;</small>";
+                            echo "</br><a href=\"http://localhost/watchstore/update.php?idproducts={$row['idproducts']}&nombre={$row['nombre']}&descripcion={$row['descripcion']}&image={$row['img']}&precio={$row['precio']}&stock={$row['stock']}\">Actualizar informacion</a>";
+                            echo "</div>";
+                            echo "</div>";
+                            echo "</div>";
+                        }
+                    }
+                    ?>
+                </div>
             </div>
-
         </div>
-    </div>
+    </header>
 
 
 <br>
